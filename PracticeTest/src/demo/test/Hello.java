095a70acda342ff0df17855ea8da5cf257c413bb
Keerthi <spachipulusu@BELR901ML8C.SEA.CORP.EXPECN.com>
2016-02-05 15:07:01 -0800

    Initial checking into git

---
+++ PracticeTest/src/demo/test/Hello.java
@@ -0,0 +1,10 @@
+package demo.test;
+
+public class Hello {
+
+	public void generate()
+	{
+		System.out.println("Hello All, How are you doing?");
+	}
+
+}
---
+++ PracticeTest/src/demo/test/Welcome.java
@@ -0,0 +1,20 @@
+package demo.test;
+
+public class Welcome {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		 Hello hi = new Hello();
+		 display();
+		 hi.generate();
+         System.out.println("Welcome To Stash");
+         
+         
+         
+	}
+	public static void display()
+    {
+   	 System.out.println("Hello All");
+    }
+    
+}
